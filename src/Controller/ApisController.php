938be4dd54e09cfacf3380d458a43900ebcd7a0a
Elsenthil <boutou.a@gmail.com>
2024-07-22 07:30:09 +0200

    update

--- src/Controller/ApisController.php
+++ src/Controller/ApisController.php
@@ -26,26 +26,123 @@ class ApisController extends AppController
         $this->request->allowMethod(['post']);
 
         $code = 500;
-        $message = 'Impossible d\'ajouter le verbe irrégulier';
+        $message = 'Erreur inconnue';
+        $success = false;
 
         $data = $this->request->getData();
         $verb = $this->fetchTable('Verbs')->newEmptyEntity();
-        // if(!empty($data)):
+        if(!empty($data)):
             
-        //     $verb->infinitive = $data['infinitive'];
-        //     $verb->preterit = $data['preterit'];
-        //     $verb->past_participle = $data['past_participle'];
-        //     $verb->translation = $data['translation'];
+            $verb->infinitive = $data['infinitive'];
+            $verb->preterit = $data['preterit'];
+            $verb->past_participle = $data['past_participle'];
+            $verb->translation = $data['translation'];
 
-        //     if($this->fetchTable('Verbs')->save($verb)):
-        //         $code = 200;
-        //         $message = 'Verbe ajouté avec succès';
-        //     else:
-        //         $message = 'Impossible d\'enregistrer le verbe';
-        //     endif;
-        // endif;
+            if($this->fetchTable('Verbs')->save($verb)):
+                $success = true;
+                $code = 200;
+                $message = 'Verbe ajouté avec succès';
+            else:
+                $message = 'Impossible d\'ajouter le verbe';
+            endif;
+        endif;
 
-        $response = ['code' => $code, 'message' => $message, 'data' => $data, 'verb' => $verb];
+        $response = ['success' => $success, 'code' => $code, 'message' => $message, 'data' => $data, 'verb' => $verb];
+
+        $this->response = $this->response->withStringBody(json_encode($response));
+        return $this->response;
+    }
+
+    public function editVerb(){
+        $this->request->allowMethod(['put']);
+
+        $code = 500;
+        $message = 'Erreur inconnue';
+        $success = false;
+
+        $data = $this->request->getData();
+        $verb = [];
+        if(!empty($data)):
+            $verb = $this->fetchTable('Verbs')->get($data['id']);
+            $verb->infinitive = $data['infinitive'];
+            $verb->preterit = $data['preterit'];
+            $verb->past_participle = $data['past_participle'];
+            $verb->translation = $data['translation'];
+
+            if($this->fetchTable('Verbs')->save($verb)):
+                $success = true;
+                $code = 200;
+                $message = 'Verbe modifié avec succès';
+            else:
+                $message = 'Impossible de modifier le verbe';
+            endif;
+        endif;
+
+        $response = ['success' => $success, 'code' => $code, 'message' => $message, 'data' => $data, 'verb' => $verb];
+
+        $this->response = $this->response->withStringBody(json_encode($response));
+        return $this->response;
+    }
+
+    public function deleteVerb(){
+        $this->request->allowMethod(['delete']);
+
+        $code = 500;
+        $message = 'Aucune données';
+        $success = false;
+
+        $data = $this->request->getData();
+        $id = $data['id'];
+        if(!empty($id)):
+            $verb = $this->fetchTable('Verbs')->get($id);
+            if ($this->fetchTable('Verbs')->delete($verb)):
+                $success = true;
+                $code = 200;
+                $message = 'Verbe irrégulier supprimé';
+            else:
+                $message = 'Impossible de supprimer le verbe irrégulier';
+            endif;
+        endif;
+
+        $response = ['success' => $success, 'code' => $code, 'message' => $message, 'data' => $data, 'verb' => $verb];
+
+        $this->response = $this->response->withStringBody(json_encode($response));
+        return $this->response;
+    }
+
+    public function deleteMultipleVerbs(){
+        $this->request->allowMethod(['delete']);
+
+        $code = 500;
+        $message = 'Erreur';
+        $success = false;
+        $data = $this->request->getData();
+        $errors = [];
+
+        if(!empty($data)):
+            foreach($data as $id):
+                $entity = $this->fetchTable('Verbs')->findById($id)->first();
+                if (!empty($entity)):
+                    $result = $this->fetchTable('Verbs')->delete($entity);
+                    if ($result):
+                        $code = 200;
+                        $success = true;
+                        $message = 'Sélection de verbe supprimée';
+                    else:
+                        $errors[] = 'Le verbe '.$entity->infinitive.' n\'a pas été supprimé';
+                    endif;
+                else:
+                    $errors[] = 'Le verbe avec l\'ID '.$id.' n\'a pas été trouvé';
+                endif;
+            endforeach;
+        endif;
+
+        if(count($errors) > 0 && $success):
+            $code = 201;
+            $message = 'Certains verbes n\'ont pas pu être supprimés';
+        endif;
+
+        $response = ['success' => $success, 'code' => $code, 'message' => $message, 'data' => $data, 'errors' => $errors];
 
         $this->response = $this->response->withStringBody(json_encode($response));
         return $this->response;
--- src/Middleware/CorsMiddleware.php
+++ src/Middleware/CorsMiddleware.php
@@ -6,15 +6,31 @@ use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\MiddlewareInterface;
 use Psr\Http\Server\RequestHandlerInterface;
 
+// src/Middleware/CorsMiddleware.php
 class CorsMiddleware implements MiddlewareInterface
 {
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
     {
-        $response = $handler->handle($request);
+        file_put_contents(WWW_ROOT.DS.'log.txt', 'Processing CORS Middleware' . PHP_EOL, FILE_APPEND);
 
         $origin = $request->getHeaderLine('Origin');
+        $allowedOrigins = ['http://localhost:3000'];
 
-        $allowedOrigins = ['*'];
+        if ($request->getMethod() === 'OPTIONS') {
+            file_put_contents(WWW_ROOT.DS.'log.txt', 'Handling OPTIONS request' . PHP_EOL, FILE_APPEND);
+
+            $response = new \Laminas\Diactoros\Response();
+            if (in_array($origin, $allowedOrigins)) {
+                $response = $response->withHeader('Access-Control-Allow-Origin', $origin)
+                                     ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+                                     ->withHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-CSRF-Token')
+                                     ->withHeader('Access-Control-Allow-Credentials', 'true');
+            }
+
+            return $response->withStatus(204);
+        }
+
+        $response = $handler->handle($request);
 
         if (in_array($origin, $allowedOrigins)) {
             $response = $response->withHeader('Access-Control-Allow-Origin', $origin)
@@ -23,10 +39,7 @@ class CorsMiddleware implements MiddlewareInterface
                                  ->withHeader('Access-Control-Allow-Credentials', 'true');
         }
 
-        if ($request->getMethod() === 'OPTIONS') {
-            return $response->withStatus(200);
-        }
-
+        file_put_contents(WWW_ROOT.DS.'log.txt', 'CORS Middleware processed' . PHP_EOL, FILE_APPEND);
         return $response;
     }
 }
